2385ca637ace3e850e42cb80556c133690aadfdc
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:23:43 +0200

    Texturapiezas.h

---
+++ Trabajo_Info_3/include/TexturaPiezas.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "freeglut.h"
+class TexturaPiezas
+{
+public:
+
+    enum IDTextura {
+        B_PEON,
+        B_TORRE,
+        B_CABALLO,
+        B_ALFIL,
+        B_REINA,
+        B_REY,
+        N_PEON,
+        N_TORRE,
+        N_CABALLO,
+        N_ALFIL,
+        N_REINA,
+        N_REY,
+        TOTAL_TEXTURAS
+    };
+
+    GLuint cargarTexturaPNG(const char* filename);
+    void cargarTodasLasTexturas(GLuint texturas[]);
+    void dibujarPieza(GLuint texID, float x, float y, float size);
+
+};
