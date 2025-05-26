ad1c655d69d948bd23119bbedbe377eafb0e9018
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:27:35 +0200

    Update Reina.h

--- Trabajo_Info_3/include/Reina.h
+++ Trabajo_Info_3/include/Reina.h
@@ -1,10 +1,16 @@
 #pragma once
-#include "Pieza.h"
+#include "pieza.h"
 
 class Reina : public Pieza {
 public:
-    using Pieza::Pieza;
+    Reina(Color c) : Pieza(c) {}
 
     char getID() const override { return 'Q'; }
+
     std::vector<Coordenada> movimientos_validos(const Coordenada& origen, const TableroLogico& t) const override;
+
+    TexturaPiezas::IDTextura getTipoTextura() const override {
+        return esPiezaBlanca() ? TexturaPiezas::B_REINA : TexturaPiezas::N_REINA;
+    }
 };
+
