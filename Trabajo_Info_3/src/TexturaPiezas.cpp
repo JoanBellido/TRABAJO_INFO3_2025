c5a0fb31c649136b1c1a9e19f425b9fcfacc8980
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:22:53 +0200

    Add files via upload

---
+++ Trabajo_Info_3/src/TexturaPiezas.cpp
@@ -0,0 +1,62 @@
+#define STB_IMAGE_IMPLEMENTATION
+#ifdef _WIN32
+#include <Windows.h>
+#endif 
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include "TexturaPiezas.h"
+#include "stb_image.h"
+#include <iostream>
+#include "pieza.h"
+
+GLuint TexturaPiezas::cargarTexturaPNG(const char* filename) {
+    int ancho, alto, canales;
+    unsigned char* data = stbi_load(filename, &ancho, &alto, &canales, STBI_rgb_alpha);
+    if (!data) {
+        std::cerr << "Error cargando: " << filename << std::endl;
+        return 0;
+    }
+
+    GLuint texID;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ancho, alto, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_image_free(data);
+    return texID;
+}
+
+void TexturaPiezas::cargarTodasLasTexturas(GLuint texturas[]) {
+    texturas[TexturaPiezas::B_PEON] = cargarTexturaPNG("piezas/b_peon.png");
+    texturas[TexturaPiezas::B_TORRE] = cargarTexturaPNG("piezas/b_torre.png");
+    texturas[TexturaPiezas::B_CABALLO] = cargarTexturaPNG("piezas/b_caballo.png");
+    texturas[TexturaPiezas::B_ALFIL] = cargarTexturaPNG("piezas/b_alfil.png");
+    texturas[TexturaPiezas::B_REINA] = cargarTexturaPNG("piezas/b_reina.png");
+    texturas[TexturaPiezas::B_REY] = cargarTexturaPNG("piezas/b_rey.png");
+
+    texturas[TexturaPiezas::N_PEON] = cargarTexturaPNG("piezas/n_peon.png");
+    texturas[TexturaPiezas::N_TORRE] = cargarTexturaPNG("piezas/n_torre.png");
+    texturas[TexturaPiezas::N_CABALLO] = cargarTexturaPNG("piezas/n_caballo.png");
+    texturas[TexturaPiezas::N_ALFIL] = cargarTexturaPNG("piezas/n_alfil.png");
+    texturas[TexturaPiezas::N_REINA] = cargarTexturaPNG("piezas/n_reina.png");
+    texturas[TexturaPiezas::N_REY] = cargarTexturaPNG("piezas/n_rey.png");
+}
+
+void TexturaPiezas::dibujarPieza(GLuint texID, float x, float y, float size) {
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texID);
+
+    glColor3f(1.0f, 1.0f, 1.0f); // Sin tinte
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 0); glVertex2f(x, y);
+    glTexCoord2f(1, 0); glVertex2f(x + size, y);
+    glTexCoord2f(1, 1); glVertex2f(x + size, y + size);
+    glTexCoord2f(0, 1); glVertex2f(x, y + size);
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D);
+};
