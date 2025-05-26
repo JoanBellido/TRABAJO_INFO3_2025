3481d017881ba65835053d3b15f3c50701d0d5a8
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:30:02 +0200

    Texturas pieza.h

--- Trabajo_Info_3/include/pieza.h
+++ Trabajo_Info_3/include/pieza.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include "TexturaPiezas.h"
 
 enum Color { BLANCO, NEGRO };
 
@@ -13,14 +14,23 @@ class TableroLogico;
 class Pieza {
 protected:
     Color color;
+    bool esBlanca;
 
 public:
-    Pieza(Color c) : color(c) {}
+    Pieza(Color c) : color(c), esBlanca(c == BLANCO) {}
     virtual ~Pieza() = default;
 
     Color getColor() const { return color; }
+    bool esPiezaBlanca() const { return esBlanca; }
+
     virtual char getID() const = 0;
     virtual std::vector<Coordenada> movimientos_validos(const Coordenada& origen, const TableroLogico& t) const = 0;
+    virtual TexturaPiezas::IDTextura getTipoTextura() const = 0;
+
+    virtual void dibujar(float x, float y, float size, GLuint texturas[]) {
+        TexturaPiezas tp;
+        tp.dibujarPieza(texturas[getTipoTextura()], x, y, size);
+    }
 };
 
 
