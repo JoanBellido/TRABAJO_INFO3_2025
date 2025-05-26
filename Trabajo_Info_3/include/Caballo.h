d8b388852ed1085e12ae6c49289d4fbdb622e6db
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:26:11 +0200

    Update Caballo.h

--- Trabajo_Info_3/include/Caballo.h
+++ Trabajo_Info_3/include/Caballo.h
@@ -1,10 +1,16 @@
 #pragma once
-#include "Pieza.h"
+#include "pieza.h"
 
 class Caballo : public Pieza {
 public:
-    using Pieza::Pieza;
+    Caballo(Color c) : Pieza(c) {}
 
     char getID() const override { return 'C'; }
+
     std::vector<Coordenada> movimientos_validos(const Coordenada& origen, const TableroLogico& t) const override;
+
+    TexturaPiezas::IDTextura getTipoTextura() const override {
+        return esPiezaBlanca() ? TexturaPiezas::B_CABALLO : TexturaPiezas::N_CABALLO;
+    }
 };
+
