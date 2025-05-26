d69d796c851974712577e057b756fe58bd34c095
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:28:04 +0200

    Update Rey.h

--- Trabajo_Info_3/include/Rey.h
+++ Trabajo_Info_3/include/Rey.h
@@ -1,10 +1,15 @@
 #pragma once
-#include "Pieza.h"
+#include "pieza.h"
 
 class Rey : public Pieza {
 public:
-    using Pieza::Pieza;
+    Rey(Color c) : Pieza(c) {}
 
     char getID() const override { return 'R'; }
+
     std::vector<Coordenada> movimientos_validos(const Coordenada& origen, const TableroLogico& t) const override;
+
+    TexturaPiezas::IDTextura getTipoTextura() const override {
+        return esPiezaBlanca() ? TexturaPiezas::B_REY : TexturaPiezas::N_REY;
+    }
 };
