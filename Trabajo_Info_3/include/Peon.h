e2e8cb2622b74276457051691925a494b02629c3
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:26:59 +0200

    Update Peon.h

--- Trabajo_Info_3/include/Peon.h
+++ Trabajo_Info_3/include/Peon.h
@@ -1,17 +1,21 @@
 #pragma once
-#include "Pieza.h"
+#include "pieza.h"
 
 class Peon : public Pieza {
 private:
     bool primerMovimiento = true;
 
 public:
-    using Pieza::Pieza;
+    Peon(Color c) : Pieza(c) {}
 
     char getID() const override { return 'P'; }
+
     std::vector<Coordenada> movimientos_validos(const Coordenada& origen, const TableroLogico& t) const override;
 
     void desactivarPrimerMovimiento() { primerMovimiento = false; }
     bool puedeDoblePaso() const { return primerMovimiento; }
-};
 
+    TexturaPiezas::IDTextura getTipoTextura() const override {
+        return esPiezaBlanca() ? TexturaPiezas::B_PEON : TexturaPiezas::N_PEON;
+    }
+};
