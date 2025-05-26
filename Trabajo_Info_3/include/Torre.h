54fcedb3a39339a71d86e3a88f140380e74e62de
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-26 23:28:58 +0200

    Textura Torre.h

--- Trabajo_Info_3/include/Torre.h
+++ Trabajo_Info_3/include/Torre.h
@@ -1,11 +1,15 @@
 #pragma once
-#include "Pieza.h"
+#include "pieza.h"
 
 class Torre : public Pieza {
 public:
-    using Pieza::Pieza;
+    Torre(Color c) : Pieza(c) {}
 
     char getID() const override { return 'T'; }
     std::vector<Coordenada> movimientos_validos(const Coordenada& origen, const TableroLogico& t) const override;
+
+    TexturaPiezas::IDTextura getTipoTextura() const override {
+        return esPiezaBlanca() ? TexturaPiezas::B_TORRE : TexturaPiezas::N_TORRE;
+    }
 };
 
