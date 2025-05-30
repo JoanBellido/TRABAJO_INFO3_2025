ab7530d1e00262ceee912e3639d8041eebc86184
NuriaGarrido <80456455+nuria0509@users.noreply.github.com>
2025-05-30 11:00:10 +0200

    Update Menu.cpp

--- Trabajo_Info_3/src/Menu.cpp
+++ Trabajo_Info_3/src/Menu.cpp
@@ -15,6 +15,23 @@ void Menu::dibujar() const {
 
     glColor3f(0.1f, 0.1f, 0.3f);
     glRasterPos2f(-2.0f, 3.0f);
+        // dibuja fondo
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glBindTexture(GL_TEXTURE_2D, texturas[TexturaPiezas::FONDO_MENU]);
+
+    glColor3f(1, 1, 1);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 0); glVertex2f(-5.5f, 5.0f);
+    glTexCoord2f(1, 0); glVertex2f(5.5f, 5.0f);
+    glTexCoord2f(1, 1); glVertex2f(5.5f, -5.0f);
+    glTexCoord2f(0, 1); glVertex2f(-5.5f, -5.0f);
+    glEnd();
+
+    glDisable(GL_BLEND);
+    glDisable(GL_TEXTURE_2D);
+//
     const char* titulo = "Ajedrez Balbo";
     for (const char* c = titulo; *c; ++c)
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
